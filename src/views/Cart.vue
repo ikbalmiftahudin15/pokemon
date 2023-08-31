d3080408a441d9b07a1db33db4a8db53e2b9d3bc
IKBALmiftahudn <ikbalmiiftahudin222>
2023-08-31 16:25:14 +0700

    pokemon

---
+++ src/views/Cart.vue
@@ -0,0 +1,104 @@
+<template>
+  <br>
+  <div class="bg-white-200">
+    <div class="grid grid-cols-4 gap-3">
+      <div v-for="product in products" :key="product.id" class="flex flex-containe">
+        
+  <a href="#" class=" m-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <span class="text-gray-600">#{{ product.id }}</span>
+    <img class=" rounded-t-lg" :src="product.imageSrc" alt="product image" />
+      <p class="mb-2 text-xl text-center font-bold tracking-tight text-red-600 dark:text-white">{{ product.name }}</p>
+     <center><div class="flex">
+        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Green</button>
+      <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
+      </div></center> 
+  </a>
+  
+      </div>
+    </div>
+  </div>
+  
+  
+  </template>
+  
+  <script setup>
+  const products = [
+      {
+        id: 1,
+        name: 'Bulbasaurus',
+        imageSrc: 'https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png',
+      },
+      {
+        id: 2,
+        name: 'Ivysaurus',
+        imageSrc: 'https://id.portal-pokemon.com/play/resources/pokedex/img/pm/3245e4f8c04aa0619cb31884dbf123c6918b3700.png',
+      },
+      {
+        id: 3,
+        name: 'Venusaurus',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+      },
+      {
+        id: 4,
+        name: 'Charmander',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      },
+      {
+        id: 5,
+        name: 'Bulbasaurus',
+        imageSrc: 'https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png',
+      },
+      {
+        id: 6,
+        name: 'Ivysaurus',
+        imageSrc: 'https://id.portal-pokemon.com/play/resources/pokedex/img/pm/3245e4f8c04aa0619cb31884dbf123c6918b3700.png',
+      },
+      {
+        id: 7,
+        name: 'Venusaurus',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+      },
+      {
+        id: 8,
+        name: 'Charmander',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      },
+      {
+        id: 9,
+        name: 'Bulbasaurus',
+        imageSrc: 'https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png',
+      },
+      {
+        id: 10,
+        name: 'Ivysaurus',
+        imageSrc: 'https://id.portal-pokemon.com/play/resources/pokedex/img/pm/3245e4f8c04aa0619cb31884dbf123c6918b3700.png',
+      },
+      {
+        id: 11,
+        name: 'Venusaurus',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+      },
+      {
+        id: 12,
+        name: 'Charmander',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      },
+      {
+        id: 13,
+        name: 'Charmander',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      },
+      {
+        id: 14,
+        name: 'Charmander',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      },
+      {
+        id: 15,
+        name: 'Charmander',
+        imageSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      },
+      
+      // More products...
+    ]
+  </script>
---
+++ src/views/Tambah.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="bg-gray-100 h-screen py-8">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row gap-4 m-10">
+                <div class="md:w-1/4">
+                    <center>
+                        <svg class="h-40 w-40 text-red-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                    </svg>
+                    </center>
+                    
+                    <p class="mb-2 text-xl text-center font-bold tracking-tight text-red-600 dark:text-white">Tambah Image</p>
+                </div>
+                <div class="md:w-3/4">
+                    <section class="bg-gray-50 dark:bg-gray-900">
+         
+              <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 class="text-xl font-bold leading-tight tracking-tight text-red-900 md:text-2xl dark:text-white">
+                      Informasi Umum
+                  </h1>
+                  <form class="space-y-4 md:space-y-6" action="#">
+                      <div>
+                          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                          <input type="email" name="email" id="email" class="bg-pink-50 border border-pink-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
+                      </div>
+                      <div>
+                          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                          <input type="password" name="password" id="password" placeholder="••••••••" class="bg-pink-50 border border-pink-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                      </div>
+                      <div>
+                          <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                          <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-pink-50 border border-pink-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                      </div>
+                      <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                      <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                          Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                      </p>
+                  </form>
+                  <a href="#" class="flex items-center justify-center rounded-md bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus:ring-4 focus:ring-blue-300">
+      
+      Tambah</a
+    >
+                  <button type="button" class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Batal</button>
+              </div>
+    </section>
+                </div>
+                </div>
+            </div>
+        </div>
+    
+    </template>
